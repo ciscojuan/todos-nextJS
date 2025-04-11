2e517afaba9fb39d7df2f09b00de98ea21e499a8
Juan Ramirez <ciscojuan@gmail.com>
2025-04-11 12:02:52 -0500

    fix: allow email linking accounts on sign in providers

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -13,74 +13,16 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_ID ?? "",
       clientSecret: process.env.GOOGLE_SECRET ?? "",
+      allowDangerousEmailAccountLinking: true,
     }),
     GithubProvider({
       clientId: process.env.GITHUB_ID ?? "",
       clientSecret: process.env.GITHUB_SECRET ?? "",
+      allowDangerousEmailAccountLinking: true,
     }),
     // ...add more providers here
   ],
-  callbacks: {
-    async signIn({ user, account, profile }) {
-      if (account?.provider === "github") {
-        const existingUser = await prisma.user.findUnique({
-          where: { email: user.email! },
-          include: { accounts: true },
-        });
 
-        if (existingUser) {
-          // Si el usuario existe pero no tiene una cuenta de GitHub vinculada
-          const hasGithubAccount = existingUser.accounts.some(
-            (acc) => acc.provider === "github"
-          );
-
-          if (!hasGithubAccount) {
-            // Vincular la cuenta de GitHub al usuario existente
-            await prisma.account.create({
-              data: {
-                userId: existingUser.id,
-                type: account.type,
-                provider: account.provider,
-                providerAccountId: account.providerAccountId,
-                access_token: account.access_token,
-                token_type: account.token_type,
-                scope: account.scope,
-                id_token: account.id_token,
-              },
-            });
-          }
-        }
-      }
-      return true;
-    },
-  },
-  pages: {
-    signIn: "/auth/signin",
-    error: "/auth/error",
-  },
-  async signIn({ user, account, profile, email, credentials }) {
-    return true;
-  },
-  async jwt({ token, user, account, profile }) {
-    const dbUser = await prisma.user.findUnique({
-      where: {
-        email: token.email ?? "no email",
-      },
-    });
-
-    token.roles = dbUser?.roles ?? ["no-roles"];
-    token.id = dbUser?.id ?? ["no-uuid"];
-
-    return token;
-  },
-
-  async session({ session, token, user }) {
-    if (session && session.user) {
-      session.user.roles = token.roles;
-      session.user.id = token.id;
-    }
-    return session;
-  },
 };
 
 const handler = NextAuth(authOptions);
